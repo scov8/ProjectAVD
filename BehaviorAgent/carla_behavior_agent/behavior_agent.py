13e748d8de6a6483da02d28a03de8f6c92456a6c
Francesco Tortora <francescotrtr@gmail.com>
2023-04-11 14:14:00 +0200

    hazard

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -231,6 +231,35 @@ class BehaviorAgent(BasicAgent):
                 self.set_destination(right_wpt.transform.location,
                                      self.end_waypoint.transform.location, clean=True)
 
+    def _bh_is_vehicle_hazard(self, ego_wpt, ego_loc, vehicle_list, proximity_th, up_angle_th, low_angle_th=0, lane_offset=0):
+
+        print('_bh_is_vehicle_hazard called')
+
+        if ego_wpt.lane_id < 0 and lane_offset != 0:
+
+            lane_offset *= -1
+
+        for target_vehicle in vehicle_list:
+
+            target_vehicle_loc = target_vehicle.get_location()
+
+            target_wpt = self._map.get_waypoint(target_vehicle_loc)
+
+            if target_wpt.road_id != ego_wpt.road_id or target_wpt.lane_id != ego_wpt.lane_id + lane_offset:
+
+                next_wpt = self._local_planner.get_incoming_waypoint_and_direction(steps=5)[
+                    0]
+
+                if target_wpt.road_id != next_wpt.road_id or target_wpt.lane_id != next_wpt.lane_id + lane_offset:
+
+                    continue
+
+            if is_within_distance(target_vehicle_loc, ego_loc, self._vehicle.get_transform().rotation.yaw, proximity_th, up_angle_th, low_angle_th):
+
+                return (True, target_vehicle, compute_distance(target_vehicle_loc, ego_loc))
+
+        return (False, None, -1)
+
     def _tailgating(self, location, waypoint, vehicle_list):
         """
         This method is in charge of tailgating behaviors.
