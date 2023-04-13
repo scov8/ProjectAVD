0b87a3544873e2aa1440a08bd5d1eecdf11ef201
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-13 22:07:53 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -147,6 +147,7 @@ class BehaviorAgent(BasicAgent):
             self._behavior.min_proximity_threshold, self._speed_limit / 3), up_angle_th=30)
         
         if front_vehicle_state and (self._speed > get_speed(front_vehicle) or get_speed(front_vehicle) == 0):
+            print("sto cazzzzoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
             if (right_turn == carla.LaneChange.Right or right_turn ==
                     carla.LaneChange.Both) and waypoint.lane_id * right_wpt.lane_id > 0 and right_wpt.lane_type == carla.LaneType.Driving:
                 new_vehicle_state, _, _ = self._vehicle_obstacle_detected(vehicle_list, max( self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=1)
