21c874d9476f6709e9bbb2c9b58ec555079ebadc
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-14 12:52:38 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -147,8 +147,7 @@ class BehaviorAgent(BasicAgent):
         
         if front_vehicle_state and (self._speed > get_speed(front_vehicle) or get_speed(front_vehicle) < 1):
             print("sto cazzzzoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
-            if (right_turn == carla.LaneChange.Right or right_turn ==
-                    carla.LaneChange.Both) and waypoint.lane_id * right_wpt.lane_id > 0 and right_wpt.lane_type == carla.LaneType.Driving:
+            if (right_turn == carla.LaneChange.Right or right_turn == carla.LaneChange.Both):
                 new_vehicle_state, _, _ = self._vehicle_obstacle_detected(vehicle_list, max( self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=1)
                 print("Massimooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
                 if not new_vehicle_state:
@@ -158,7 +157,7 @@ class BehaviorAgent(BasicAgent):
                     self._behavior.overtake_counter = 200
                     self.set_destination(end_waypoint.transform.location, right_wpt.transform.location) 
 
-            elif left_turn == carla.LaneChange.Left and waypoint.lane_id * left_wpt.lane_id > 0 and left_wpt.lane_type == carla.LaneType.Driving:
+            elif left_turn == carla.LaneChange.Left or left_turn == carla.LaneChange.Both:
                 new_vehicle_state, _, _ = self._vehicle_obstacle_detected(vehicle_list, max( self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=-1)
                 print("Massimooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
                 if not new_vehicle_state:
