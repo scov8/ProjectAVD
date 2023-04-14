fa9fd840c63a69169a746189799656c4dbeaa593
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-14 13:50:43 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -140,7 +140,8 @@ class BehaviorAgent(BasicAgent):
         if front_vehicle_state and (self._speed > get_speed(front_vehicle) or get_speed(front_vehicle) < 1):
             if self._behavior.overtake_counter == 0:
                 new_vehicle_state, _, _ = self._vehicle_obstacle_detected(vehicle_list, max( self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=-1)
-                if not new_vehicle_state:
+                new_vehicle_state2, _, _ = self._vehicle_obstacle_detected(vehicle_list, max( self._behavior.min_proximity_threshold, self._speed_limit * 2), up_angle_th=40, lane_offset=-1)
+                if not new_vehicle_state and not new_vehicle_state2:
                     self._behavior.overtake_counter == 1
                     self.lane_change("left")
             else:
@@ -260,7 +261,7 @@ class BehaviorAgent(BasicAgent):
         print(distance)
 
         # if mio
-        if (vehicle_speed < self._speed or vehicle_speed < 1.0) and distance < 15.0:
+        if (vehicle_speed < (self._speed / 3) or vehicle_speed < 1.0) and distance < 15.0:
             wpt = ego_vehicle_wp.get_left_lane()    
             if self._behavior.overtake_counter == 0:
                 self._overtake(vehicle_list)
