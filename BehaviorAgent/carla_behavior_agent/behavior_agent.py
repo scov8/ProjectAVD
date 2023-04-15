928e467eb7c3e9f54ad268ab99e2ecf7692dbd37
Francesco Tortora <francescotrtr@gmail.com>
2023-04-15 14:44:58 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -146,8 +146,8 @@ class BehaviorAgent(BasicAgent):
             if not new_vehicle_state and not new_vehicle_state2:
                 self._behavior.overtake_doing = 1
                 self._behavior.overtake_counter = 100
-                self.lane_change("left", other_lane_time=2, overtake_do=True)
-                self._local_planner.set_speed(70)
+                self.lane_change("left", other_lane_time=4, overtake_do=True)
+                self._local_planner.set_speed(130)
         elif self._behavior.overtake_doing == 1 and self._behavior.overtake_counter == 0:
             new_vehicle_state, _, _ = self._vehicle_obstacle_detected(to_overtake, max(
                 self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=180, lane_offset=1)
