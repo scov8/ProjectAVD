579723f29827cf8e2e7fc820eded99a430233f82
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-18 09:49:26 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -389,10 +389,9 @@ class BehaviorAgent(BasicAgent):
                 pass
             else:
                 # faccio rallentare la macchina
-                #target_speed = self._speed - self._behavior.speed_decrease
-                #self._local_planner.set_speed(target_speed)
-                #control = self._local_planner.run_step(debug=debug)
-                pass
+                target_speed = self._speed - (self._behavior.speed_decrease-3)
+                self._local_planner.set_speed(target_speed)
+                control = self._local_planner.run_step(debug=debug)
 
         # 2.2: Car following behaviors
         vehicle_state, vehicle, distance = self.collision_and_car_avoid_manager(ego_vehicle_wp)
