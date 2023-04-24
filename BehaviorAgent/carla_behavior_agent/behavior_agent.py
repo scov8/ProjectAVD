b69127866d7826907f42d30287fde395b9f08a87
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-24 17:56:33 +0200

    Update behavior_agent.py
    add in the pedestrian avoid manager the control if we find a walker

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -209,6 +209,9 @@ class BehaviorAgent(BasicAgent):
         walker_list = [w for w in walker_list if dist(w) < 10]
         walker_list.sort(key=dist)
 
+        if walker_list == []:
+            return False, None, None
+
         if self._direction == RoadOption.CHANGELANELEFT:
             walker_state, walker, distance = self._vehicle_obstacle_detected(walker_list, max(
                 self._behavior.min_proximity_threshold, self._speed_limit / 2), up_angle_th=90, lane_offset=-1)
