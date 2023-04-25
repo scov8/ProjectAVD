ec5976f7af93dc2c2f30749aa959f5bc8d4a0a13
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-25 13:26:02 +0200

    Update basic_agent.py

--- BehaviorAgent/carla_behavior_agent/basic_agent.py
+++ BehaviorAgent/carla_behavior_agent/basic_agent.py
@@ -659,7 +659,7 @@ class BasicAgent(object):
         while distance < distance_same_lane:
             next_wps = plan[-1][0].next(step_distance)
             if not next_wps:
-                return [] #plan
+                return plan #[]
             next_wp = next_wps[0]
             distance += next_wp.transform.location.distance(plan[-1][0].transform.location)
             plan.append((next_wp, RoadOption.LANEFOLLOW))
@@ -670,7 +670,7 @@ class BasicAgent(object):
             option = RoadOption.CHANGELANERIGHT
         else:
             # ERROR, input value for change must be 'left' or 'right'
-            return [] #plan
+            return plan #[]
 
         lane_changes_done = 0
         lane_change_distance = lane_change_distance / lane_changes
@@ -682,28 +682,28 @@ class BasicAgent(object):
         while lane_changes_done < lane_changes:
 
             # Move forward
-            #if curr_orientation != side_orientation and direction == "left":
-            #    next_wps = plan[-1][0].previous(step_distance)
-            #else:
-            #    next_wps = plan[-1][0].next(step_distance)
+            if curr_orientation != side_orientation and direction == "left":
+                next_wps = plan[-1][0].previous(step_distance)
+            else:
+                next_wps = plan[-1][0].next(step_distance)
 
-            next_wps = plan[-1][0].next(lane_change_distance) # prima dell'if era così
+            #next_wps = plan[-1][0].next(lane_change_distance) # prima dell'if era così
             if not next_wps:
-                return [] #plan
+                return plan #[]
             next_wp = next_wps[0]
 
             # Get the side lane
             if direction == 'left':
                 if check and str(next_wp.lane_change) not in ['Left', 'Both']:
-                    return []
+                    return plan #[]
                 side_wp = next_wp.get_left_lane()
             else:
                 if check and str(next_wp.lane_change) not in ['Right', 'Both']:
-                    return []
+                    return plan #[]
                 side_wp = next_wp.get_right_lane() # provare anche con solo next_wp 
 
             if not side_wp or side_wp.lane_type != carla.LaneType.Driving:
-                return []
+                return plan #[]
 
             # Update the plan
             plan.append((side_wp, option))
@@ -718,7 +718,7 @@ class BasicAgent(object):
                 next_wps = plan[-1][0].next(step_distance)
             
             if not next_wps:
-                return [] #plan
+                return plan #[]
             next_wp = next_wps[0]
             distance += next_wp.transform.location.distance(plan[-1][0].transform.location)
             plan.append((next_wp, RoadOption.LANEFOLLOW))
