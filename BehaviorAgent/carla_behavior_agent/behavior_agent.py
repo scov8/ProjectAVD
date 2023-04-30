71057bb9b0f8f3e0fba775cfa37a0efe6716472c
Luigi Scovotto <luigiscovotto@libero.it>
2023-04-30 11:01:52 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -442,7 +442,7 @@ class BehaviorAgent(BasicAgent):
         elif self._overtaking:
             print("sorpasso in corso...")
             if not self._local_planner.has_incoming_waypoint():
-                if not self._other_lane_occupied(ego_vehicle_loc, 15, check_behind=True):
+                if not self._other_lane_occupied(ego_vehicle_loc, 30, check_behind=True):
                     print("RIENTRO")
                     if self.lane_change("left", self._vehicle_heading, 0, 2, 2):
                         self._ending_overtake = True
