b658f6b621d53d33cb5d100539cd493c1b228d6e
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-01 13:22:30 +0200

    stay on right

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -386,15 +386,8 @@ class BehaviorAgent(BasicAgent):
         if vehicle_state_invasion:
             invasion_state, offset_invasion = self._lane_invasion(self._vehicle, vehicle_invasion, ego_vehicle_loc)
             if invasion_state:
-
-                vehicle_state_invasion, vehicle_invasion = self._other_lane_occupied_bis(ego_vehicle_loc, distance=50)
-                if vehicle_state_invasion:
-                    invasion_state, offset_invasion = self._lane_invasion(self._vehicle, vehicle_invasion, ego_vehicle_loc)
-                    if invasion_state:
-                        return self.emergency_stop()
-                    
                 print('LANE INVASION: TRUE, SO DO EMERGENCY STOP')
-                self.stay_on_the_right(ego_vehicle_wp, offset_invasion, 2)
+                self.stay_on_the_right(ego_vehicle_wp, offset_invasion+3, 2)
                 target_speed = min([self._behavior.max_speed, self._speed_limit]) - (self._behavior.speed_decrease * 3)
                 self._local_planner.set_speed(target_speed)
                 control = self._local_planner.run_step(debug=debug)
