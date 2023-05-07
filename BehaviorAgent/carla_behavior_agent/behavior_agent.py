08ebd368209d4567016bdf9a24b1205f214a0c80
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-07 19:03:42 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -475,7 +475,7 @@ class BehaviorAgent(BasicAgent):
                         new_vehicle_state2, _, _ = self._vehicle_obstacle_detected(vehicle_list, max(self._behavior.min_proximity_threshold, self._speed_limit), low_angle_th= 90, up_angle_th=180, lane_offset=-1) # new
                         if not new_vehicle_state and not new_vehicle_state2:
                             if not self._other_lane_occupied(ego_vehicle_loc, distance=70) and not self._overtaking:
-                                if self.lane_change("left", self._vehicle_heading, 0, 2, 1.5): # new: 2 al posto di 1.5
+                                if self.lane_change("left", self._vehicle_heading, 0, 2, 2):
                                     self._overtaking = True
                                     target_speed = max([self._behavior.max_speed, self._speed_limit])
                                     self._local_planner.set_speed(target_speed)
