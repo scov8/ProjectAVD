26e24de10e568f61b40fc714997b6d4dca49c3f3
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-07 14:46:52 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -376,7 +376,7 @@ class BehaviorAgent(BasicAgent):
                 self.stay_on_the_right(ego_vehicle_wp, offset_invasion-2, 2) #con -2 va  ma struscia, con 3 fa inverisone
                 #self._local_planner.set_lat_offset(offset_invasion) # mio
                 self._restringimento = True
-                target_speed = min([self._behavior.max_speed, self._speed_limit]) - (self._behavior.speed_decrease * 3)
+                target_speed = min([self._behavior.max_speed, self._speed_limit]) - (self._behavior.speed_decrease * 3.5)
                 self._local_planner.set_speed(target_speed)
                 control = self._local_planner.run_step(debug=debug)
                 return control
