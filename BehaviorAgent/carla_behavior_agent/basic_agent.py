e6ab522479cbb0b49c29bc014a950d8092b2c358
Francesco Tortora <francescotrtr@gmail.com>
2023-05-08 14:50:29 +0200

    try

--- BehaviorAgent/carla_behavior_agent/basic_agent.py
+++ BehaviorAgent/carla_behavior_agent/basic_agent.py
@@ -598,7 +598,7 @@ class BasicAgent(object):
                 target_transform.location, lane_type=carla.LaneType.Any)
             print("Target wpt: ", target_wpt.lane_id)
             if not ego_wpt.is_junction or not target_wpt.is_junction:
-                if target_wpt.road_id != ego_wpt.road_id or target_wpt.lane_id != ego_wpt.lane_id + lane_offset:
+                if target_wpt.road_id != ego_wpt.road_id or target_wpt.lane_id != ego_wpt.lane_id + lane_offset or lane_offset == 0 and target_wpt.lane_id != ego_wpt.lane_id + 1:
                     next_wpt = self._local_planner.get_incoming_waypoint_and_direction(steps=3)[
                         0]
                     if not next_wpt:
