a9511e35980cf46b342dbc8462de82f75e084d2d
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-12 13:30:29 +0200

    prova r0

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -498,10 +498,10 @@ class BehaviorAgent(BasicAgent):
         # 3: Intersection behavior
         elif self._incoming_waypoint.is_junction and (self._incoming_direction in [RoadOption.LEFT, RoadOption.RIGHT]):
             # controllo comportamento in caso di incrocio con segnale di stop e controllo se il veicolo non è già fermo
-            if self.stop_signs_manager(ego_vehicle_wp) and not get_speed(self._vehicle) < 1.0:
-                print('--------------- [stop] ------------------')
+            #if self.stop_signs_manager(ego_vehicle_wp) and not get_speed(self._vehicle) < 1.0:
+            #    print('--------------- [stop] ------------------')
                 #return self.decelerate()
-                return self.emergency_stop()
+            #    return self.emergency_stop()
             target_speed = min([self._behavior.max_speed, self._speed_limit])
             self._local_planner.set_speed(target_speed)
             control = self._local_planner.run_step(debug=debug)
