c7a6d2a0377f7762fb2a85065d57cbd48e2bc935
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-14 14:53:34 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -437,7 +437,7 @@ class BehaviorAgent(BasicAgent):
                 self._overtaking = False
                 self._overtaking_obj = False
                 #route_trace = self.trace_route(self._wp_overtake, self._destination_waypoint)
-                
+                print("coda ", self._waypoints_queue_copy)
                 def primo_elemento(t):
                     return t[0]
                 route_trace_p = list(map(primo_elemento, self._waypoints_queue_copy))
