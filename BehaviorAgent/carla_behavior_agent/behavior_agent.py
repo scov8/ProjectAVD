f53900a56618bf048efb4c2b6328ba390f12d8a9
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-15 17:30:17 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -396,6 +396,8 @@ class BehaviorAgent(BasicAgent):
         # 1: Red lights and stops behavior
         if self.traffic_light_manager():
             return self.emergency_stop()
+        
+        self._local_planner.set_lat_offset(3)
 
         # 2.3: Lane Invasion (degli altri)
         vehicle_state_invasion, vehicle_invasion = self._other_lane_occupied_lane_invasion(distance=30)
@@ -412,7 +414,7 @@ class BehaviorAgent(BasicAgent):
                 return control
         elif self._shrinkage:
             print('LANE INVASION: FALSE')
-            self._local_planner.set_lat_offset(0.0)
+            #self._local_planner.set_lat_offset(0.0)
             route_trace = self.trace_route(ego_vehicle_wp, self._destination_waypoint)
             self._local_planner.set_global_plan(route_trace,  clean_queue=True)
             self._shrinkage = False
