967ec3816d7d8cbfd81889a29a0c2788ee276d9b
Luigi Scovotto <luigiscovotto@libero.it>
2023-05-16 15:24:37 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -180,10 +180,19 @@ class BehaviorAgent(BasicAgent):
         def dist(v): return v.get_location().distance(waypoint.transform.location)
         vehicle_list = [v for v in vehicle_list if dist(v) < 30 and v.id != self._vehicle.id and (self._map.get_waypoint(v.get_transform().location).lane_id == ego_wpt.lane_id or (abs(self._map.get_waypoint(v.get_transform().location).lane_id) == abs(ego_wpt.lane_id) + 1))]
         vehicle_list.sort(key=dist)
-        # nel for elimino i veicoli che stanno dietro a me come per le junction
+
+        vehicle_location = self._vehicle.get_location()
+        vehicle_yaw = math.radians(self._vehicle.get_transform().rotation.yaw)
+        closest_distance = float('inf')
         for v in vehicle_list:
-            if self._map.get_waypoint(v.get_transform().location).transform.location.distance(waypoint.transform.location) < 0:
+            v_location = v.transform.location
+            v_direction = math.atan2(v_location.y - vehicle_location.y, v_location.x - vehicle_location.x)
+            intersection_distance = math.sqrt((v_location.x - vehicle_location.x)**2 + (v_location.y - vehicle_location.y)**2)
+            relative_direction = abs(math.degrees(vehicle_yaw - v_direction))
+            if relative_direction <= 90 and intersection_distance < closest_distance:
+                closest_distance = intersection_distance
                 vehicle_list.remove(v)
+
         if len(vehicle_list) == 0:
             return False, 0, 0
         else:
