efb8c6d7ee18d4357628fb46cb770e5b4260d990
Francesco Tortora <francescotrtr@gmail.com>
2023-05-19 11:47:51 +0200

    Update behavior_agent.py

--- BehaviorAgent/carla_behavior_agent/behavior_agent.py
+++ BehaviorAgent/carla_behavior_agent/behavior_agent.py
@@ -196,9 +196,9 @@ class BehaviorAgent(BasicAgent):
             v2_location = vehicle_list[i+1].get_transform().location
             v_distance = math.sqrt((v2_location.x - v1_location.x)**2 + (v2_location.y - v1_location.y)**2)
             #print("VEICOLO: ", v, "DISTANZA: ", v.get_location().distance(self._vehicle.get_location()))
+            print("VEICOLO: ", vehicle_list[i], "DISTANZA: ", v_distance)
+            print("i: ", i, "len: ", len(vehicle_list))
             if v_distance < 15:
-                print("VEICOLO: ", vehicle_list[i], "DISTANZA: ", v_distance)
-                print("i: ", i, "len: ", len(vehicle_list))
                 v_list.append(vehicle_list[i])
                 distance = v2_location.distance(self._vehicle.get_location())
                 if v_distance > d_max:
